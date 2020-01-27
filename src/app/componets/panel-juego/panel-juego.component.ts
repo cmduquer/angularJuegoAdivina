e8c5c8e25b3d920810bf4b17de75da76fbe3a5f4
Carlos Mauricio Duque Restrepo <cmduquer@mbp-de-carlos-2.home>
2020-01-27 21:43:47 +0000

    Parte2

--- src/app/componets/panel-juego/panel-juego.component.ts
+++ src/app/componets/panel-juego/panel-juego.component.ts
@@ -6,16 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel-juego.component.css']
 })
 export class PanelJuegoComponent implements OnInit {
-
+  valorJuego: number;
   vidas: number = 5;
   numeros: Array<number> = [];
-  valorIngresado: number = 5;
+  valorIngresado: number;
   resultado: string = 'En curso';
   finalizado: boolean = false;
+  ganador: boolean = false;
+  pista: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.valorJuego = Math.floor(Math.random() * (101));
+  }
+  
+  intento(): void {
+    if(this.valorIngresado == this.valorJuego) {
+      this.ganador = true;
+      this.finalizado = true;
+      this.pista = 'Igual';
+    }
+    if(this.valorIngresado > this.valorJuego) {
+      this.pista = 'Mi número es menor';
+    }
+    if(this.valorIngresado < this.valorJuego) {
+      this.pista = 'Mi número es mayor';
+    }
+    this.numeros.push(this.valorIngresado);
+    console.log(this.numeros);
+    this.vidas = this.vidas - 1;
+    if(this.vidas === 0){
+      this.finalizado = true;
+    }
+  }
+
+  reinicio() : void {
+    this.valorJuego = Math.floor(Math.random() * (101));  
+    this.vidas = 5;
+    this.numeros = [];
+    this.valorIngresado = null;
+    this.finalizado = false;
+    this.ganador = false;
+    this.pista = '';
   }
 
 }
