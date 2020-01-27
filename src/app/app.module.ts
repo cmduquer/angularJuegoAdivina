df9075d7c064f443216ba70b4f5d37d28edd9406
cmduquer <cmduquer@gmail.com>
2020-01-27 23:06:47 +0000

    fix path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PanelJuegoComponent } from './componets/panel-juego/panel-juego.component';
+import { PanelJuegoComponent } from './components/panel-juego/panel-juego.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
